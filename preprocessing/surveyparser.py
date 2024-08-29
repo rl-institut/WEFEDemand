69db212742d52e45e0bd8dfde0cdb42e48e89898
EdL <giovanni.costantini92@gmail.com>
2024-08-29 14:23:41 +0200

    created new class to handle surveys

--- preprocessing/surveyparser.py
+++ preprocessing/surveyparser.py
@@ -1,10 +1,12 @@
 import os
 import json
+import warnings
+
 from copy import copy
 
 from preprocessing.formparser import FormParser
 from preprocessing.utils import load_kobo_data
-
+from preprocessing import constants
 
 def process_survey(
     surv_id: str,
@@ -63,7 +65,7 @@ def process_survey(
             a_count += 1
             form_name = f"{parser.formtype}_{a_count}"
         print(form_name)
-        if parser.formtype == "business" and b_count not in [1,2,4,5,6,7,8,9,10]:
+        if parser.formtype == "business" and b_count not in [1,2,3,4,5,6,7,8,9,10]:
             try:
                 parser.create_dictionary()
             except:
@@ -89,29 +91,79 @@ def process_survey(
         return output_dict
 
 class SurveyParser:
-    def __init__(self, survey_key: str, token: str) -> None:
+    def __init__(self, survey_key=None, token=None) -> None:
         self.formparser = FormParser()
-        self.suvey_key = survey_key
+        self.survey_key = survey_key
+        self.survey = None
         self.token = token
-
-        if self.suvey_key is None or self.token is None:
+        self.n_forms = {constants.formtype_names[i]: 0 for i in range(len(constants.formtype_names))}
+        self.id_forms = {constants.formtype_names[i]: [] for i in range(len(constants.formtype_names))}
+        self.forms = {}
+        self.local_aut = None
+        self.morethanone = False
+        if self.survey_key is None or self.token is None:
             try:
                 self.init_parser()
             except:
                 raise ValueError("Survey key and kobo api token not given and not found in system environment")
             
     def init_parser(self) -> None:
-        pass
+        self.survey_key, self.token = os.getenv('SURVEY_KEY'), os.getenv('KOBO_TOKEN')
 
     def read_survey(self) -> None:
-        temp_form = {}
-        for form in load_kobo_data(self.survey_key, self.token):
-            self.formparser.check_form_type(form)
+        self.survey, _  = load_kobo_data(self.survey_key, self.token)
+        for form in self.survey:
+            self.formparser.init_parser(form)
+            type = self.formparser.formtype
+            self.n_forms[type] = self.n_forms[type] + 1
+            self.id_forms[type].append(form['_id'])
+            self.forms[form['_id']] = form
+            if type == "local_aut":
+                if not self.morethanone:
+                    self.local_aut = form
+                    self.morethanone = True
+                else:
+                    print("WARNING: More than one Local Authority form found. Will use the first one: " + self.local_aut['_id'] + ".")
 
-    def process_survey(self, survey_key: str, token: str) -> None:
+    def process_survey(self, form_type=None, form_id=None) -> None:
+        if self.local_aut is not None:
+            self.formparser.init_parser(self.local_aut)
+            #self.summary = self.formparser.create_dictionary()
+        else:
+            warnings.warn("WARNING: No Local Authority form defined. Numerisity set to 1 for all forms.")
+
+        self._match_localaut_info_to_survey()
+
+        output = {}
+
+        if self.survey is not None:
+            if form_id is not None:
+                for id in form_id:
+                    self.formparser.init_parser(self.forms[id])
+                    output[id] = self.formparser.create_dictionary()
+            elif form_type is not None:
+                for id in self.id_forms[form_type]:
+                    try:    
+                        self.formparser.init_parser(self.forms[id])
+                        output[id] = self.formparser.create_dictionary()
+                        print(f"Processed form {id}.")
+                    except:
+                        print(f"WARNING: Could not process form {id}"
+                              " Skipping this form.")
+                        continue
+            elif form_type is None and form_id is None:
+                for form in self.survey:
+                    try:    
+                        self.formparser.init_parser(form)
+                        output[form['_id']] = self.formparser.create_dictionary()
+                    except:
+                        print(f"WARNING: Could not process form {form['_id']}"
+                              " Skipping this form.")
+                        continue
+        else:
+            raise BaseException("WARNING: No survey data defined.")
+        
+        return output
+
+    def _match_localaut_info_to_survey(self) -> None:
         pass
-
-
-if __name__ == "__main__":
-    process_survey(os.getenv('SURVEY_KEY'), os.getenv('KOBO_TOKEN'))
- 
