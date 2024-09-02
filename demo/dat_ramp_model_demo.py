427e254654c45d1ac3e14194edcee51ee2a5a63f
JulianBarinton <julianfleischmann@gmail.com>
2024-09-02 15:22:41 +0200

    adding and rearranging code line structure for creating aggregated WEFE demand output sequences

--- demo/dat_ramp_model_demo.py
+++ demo/dat_ramp_model_demo.py
@@ -9,6 +9,24 @@ from input.admin_input import admin_input
 ramp_control = RampControl(365, "2018-01-01")
 
 dat_output = ramp_control.run_opti_mg_dat(input_dict, admin_input)
+
+# %% Plot aggregated demands
+
+agg_demand = dat_output.groupby(level=0, axis=1).sum()
+
+# Output the aggregated demand to a CSV file
+agg_demand.to_csv("agg_demand_Aiwa.csv")
+print("Aggregated demand data has been written to csv")
+
+# Display the first few rows of the aggregated demands
+agg_demand.head()
+print(agg_demand.head())
+
+# Create Plots in Dash
+fig = make_subplots(rows=1, cols=1, shared_xaxes=True)
+fig = plotting.plotly_high_res_df(fig, df=agg_demand, subplot_row=1)
+fig.show_dash(mode="external")
+
 # %% Plot raw output
 fig = make_subplots(rows=5, cols=1, shared_xaxes=True)
 
@@ -22,11 +40,3 @@ fig.update_layout(autosize=True)
 
 fig.show_dash(mode="external")
 
-# %% Plot aggregated demands
-
-agg_demand = dat_output.groupby(level=0, axis=1).sum()
-agg_demand.head()
-
-fig = make_subplots(rows=1, cols=1, shared_xaxes=True)
-fig = plotting.plotly_high_res_df(fig, df=agg_demand, subplot_row=1)
-fig.show_dash(mode="external")
