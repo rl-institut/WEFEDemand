9ba0a268a758167125972615206b1e2683300ae4
paulapreuss <paulavpreuss@outlook.com>
2025-09-16 18:14:50 +0200

    Delete preprocessing endpoint and add index
    Only ramp simulation endpoint should be necessary, since it already calls preprocessing

--- demo/app.py
+++ demo/app.py
@@ -5,34 +5,9 @@ from ramp_simulation_demo import main as run_ramp_simulation
 
 app = Flask(__name__)
 
-def run_script(script, args):
-    try:
-        args_list = []
-        for k, v in args.items():
-            if isinstance(v, list):
-                args_list.append(f"--{k}")
-                args_list.extend(str(x) for x in v)
-            else:
-                args_list.append(f"--{k}={v}")
-        print(script, args_list)
-        result = subprocess.run(
-            ["python", script, *args_list],
-            capture_output=True,
-            text=True
-        )
-        return jsonify({"stdout": result.stdout, "stderr": result.stderr, "exit_code": result.returncode})
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-@app.route("/preprocessing", methods=["POST"])
-def run_preprocessing():
-    data = request.get_json()
-    script = "demo/preprocessing_demo.py"
-    args = data.get("args")
-    survey_id = data.get("survey_id")
-    os.environ["SURVEY_KEY"] = survey_id
-    res = run_script(script, args)
-    return res
+@app.route('/')
+def index():
+    return jsonify({"message": "Welcome to the WEFEDemand app, please use the /ramp-simulation endpoint to perform simulations"})
 
 @app.route("/ramp-simulation", methods=["POST"])
 def run_simulation():
